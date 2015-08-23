325b1ed28307a66a1d01ac65ea0da96471fd62a1
Manuel Voss <mv@moay.de>
2015-08-23 17:12:34 +0200

    Cleanup

--- gulpfile.js
+++ gulpfile.js
@@ -2,50 +2,49 @@
 
 var plugins = require('gulp-load-plugins')();
 var gulp = require('gulp');
+var fs = require('fs');
+var argv = require('yargs').argv;
 
 gulp.task('build', function(){
 	// Loading LESS files 
 	return gulp.src([
-        "./src/videojs/skin/afterglow/vjs-afterglow.less"
-    ])
- 
-    // Convert LESS files to CSS 
-    .pipe(plugins.less())
+		"./src/videojs/skin/afterglow/vjs-afterglow.less"
+		])
 
-    // Add normal css which doesn't need to be compiled
-    .pipe(plugins.addSrc.prepend('./src/videojs/video-js.css'))
- 
-    // Minify the CSS 
-    .pipe(plugins.cssmin())
- 
-    // Now convert it to JavaScript and specify options 
-    .pipe(plugins.css2js({
-        splitOnNewline: false
-    }))
+	// Convert LESS files to CSS 
+	.pipe(plugins.less())
 
-    // Add all the javascript files in the correct order
-    .pipe(plugins.addSrc.append([
-        './src/dollardom/dollardom.min.js',
-        './src/videojs/video.js',
-    ]))
-    .pipe(plugins.addSrc.append([
-        './src/videojs/ie8/videojs-ie8.js',
-        './src/videojs/plugins/videojs.hotkeys.js',
-        './src/videojs/plugins/Youtube.js',
-    ]))
-    .pipe(plugins.addSrc.append([
-        './src/afterglow.js'
-    ]))
+	// Add normal css which doesn't need to be compiled
+	.pipe(plugins.addSrc.prepend('./src/videojs/video-js.css'))
+	
+	// Minify the CSS 
+	.pipe(plugins.cssmin())
+	
+	// Now convert it to JavaScript and specify options 
+	.pipe(plugins.css2js({
+		splitOnNewline: false
+	}))
 
-    // Concatenate into a single large file 
-    .pipe(plugins.concat("afterglow.min.js"))
- 
-    // Minify the JavaScript 
-    .pipe(plugins.uglifyjs())
- 
-    // Finally write it to our destination (./dist/afterglow.min.js) 
-    .pipe(gulp.dest("./dist/"));
+	// Add all the javascript files in the correct order
+	.pipe(plugins.addSrc.append([
+		'./src/dollardom/dollardom.min.js',
+		'./src/videojs/video.js',
+		]))
+	.pipe(plugins.addSrc.append([
+		'./src/videojs/ie8/videojs-ie8.js',
+		'./src/videojs/plugins/videojs.hotkeys.js',
+		'./src/videojs/plugins/Youtube.js',
+		]))
+	.pipe(plugins.addSrc.append([
+		'./src/afterglow.js'
+		]))
+
+	// Concatenate into a single large file 
+	.pipe(plugins.concat("afterglow.min.js"))
+	
+	// Minify the JavaScript 
+	.pipe(plugins.uglifyjs())
+	
+	// Finally write it to our destination (./dist/afterglow.min.js) 
+	.pipe(gulp.dest("./dist/"));
 });
-
-// Get release tasks
-require('gulp-release-tasks')(gulp);
